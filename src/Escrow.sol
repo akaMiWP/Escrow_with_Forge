557b8a7b5525d2c9a839d9296a1e5fc1f255c925
akaMiWP <bangkokappdevelopment@gmail.com>
2024-06-02 10:06:41 +0700

    Added Escrow contract

---
+++ src/Escrow.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Escrow {
+
+    address public buyer;
+    address public seller;
+    address public inspector;
+
+    struct Shipping {
+        bool isOrderCompleted;
+        string shippingAddress;
+    }
+
+    Shipping public shipping;
+    bool public isEscrowCompleted;
+    uint public productPrice;
+
+    constructor(address _seller, address _inpsector, uint _productPrice) payable {
+        buyer = msg.sender;
+        seller = _seller;
+        inspector = _inpsector;
+        productPrice = _productPrice;
+        require(msg.value >= productPrice, "Escrow is not correctly set");
+    }
+
+    function confirmOrder(Shipping calldata _shipping) external {
+        require(!isEscrowCompleted, "Escrow has been completed");
+        require(msg.sender == seller, "Only the seller that is able to confirm the order");
+        shipping = _shipping;
+    }
+
+    function completeEscrow() external {
+        require(!isEscrowCompleted, "Escrow has been completed");
+        require(msg.sender == inspector, "Reserved only for the inspector");
+        require(shipping.isOrderCompleted, "An order hasn't been completed");
+        (bool s, ) = seller.call{value: productPrice}("");
+        require(s, "Unable to send money");
+        isEscrowCompleted = true;
+    }
+}
